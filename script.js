512daee8372aa7a4ae4b6856089b4f603bb2fbcf
Adarsh <adarshofficial.1607@gmail.com>
2023-12-19 01:39:43 +0530

    Responsiveness

---
+++ script.js
@@ -0,0 +1,10 @@
+const slideBtn = document.querySelector(".slide__btn");
+const closeBtn = document.querySelector(".close__btn");
+const slideBar = document.querySelector(".navbar");
+
+slideBtn.addEventListener("click", () => {
+  slideBar.style.right = 0;
+  closeBtn.addEventListener("click", () => {
+    slideBar.style.right = '-250px';
+  })
+});
